e957470c0234e8e9551f2dba6f9f3f8d4c764859
youssef ifkiren <66288564+youssefifkiren@users.noreply.github.com>
2022-02-01 18:36:55 +0100

    addition

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,57 @@
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+const MaStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white"
+const ConnectMeta = () => {
+    
+}
 const Home = () => {
     return (
-        <h1>Home</h1>
-    );
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Envoyer Crypto <br />à travers le monde</h1>
+                    <p className="text-left text-white mt-5 font-light w-9/12 w-11/12 text-base">explorez le monde de la cryptographie, achetez et vendez facilement Crypto avec cryptoWB</p>
+                    <button
+                    type="button"
+                    onClick={ConnectMeta}
+                    className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] text-white text-base font-semibold"
+                    >Connectez votre portefeuille</button>
+                    <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+                        <div className={`rounded-tl-2xl ${MaStyles}`}>fiabilité</div>
+                        <div className={`rounded-tr-2xl sm:rounded-none ${MaStyles}`}>Blockchain</div>
+                        <div className={`sm:rounded-tr-2xl rounded-none ${MaStyles}`}>Web 3.0</div>
+                        <div className={`sm:rounded-bl-2xl rounded-none ${MaStyles}`}>EtherNet</div>
+                        <div className={`rounded-bl-2xl sm:rounded-none ${MaStyles}`}>Sécurité</div>
+                        <div className={`rounded-br-2xl ${MaStyles}`}>Low fees</div>
+
+                    </div>
+                </div>
+                <div className="flex flex-col flex-1 justify-start items-center w-full md:mt-0 mt-10">
+                    <div className="p-3 justify-end items-start flex-col rounded-xl h-40 my-5 sm:w-80 w-full eth-card white-glassmorphism">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                    <SiEthereum fontSize={24} color="#FFFFFF"/>
+                                </div>
+                                <BsInfoCircle fontSize={16} color="#FFFFFF"/>
+                            </div>
+                            <div>
+                                <p className="text-white font-light  text-xs">0x17B44E4936D2AA2F569D4F31bD80d2198f7E889E</p>
+                                <p className="text-white font-semibold text-lg">Ethereum</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                        <form action="#" method="post">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
 }
 
 export default Home;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
 import logo from '../../images/logo.png';
+import React from 'react';
 
 const NavItem = ({title, Properties}) => {
     return (
@@ -11,18 +12,39 @@ const NavItem = ({title, Properties}) => {
     );
 }
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = React.useState(false);
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-inital justify-center items-center">
-                <img src={logo} alt="CryptoWB" className="w-32 cursor-pointer" />
+                {/* <img src={logo} alt="CryptoWB" className="w-32 cursor-pointer" /> */}
             </div>
-            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-inital">
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-inital">
                 {["Marché", "Echanges", "Coures", "Port-feullies"].map( (item, id) => (
                     <NavItem key={item + id} title={item} />
                 ) )}
                 <li className="bg-[#2955f3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2440b0]">Login</li> 
             </ul>
+            <div className="flex relative">
+                {toggleMenu ? 
+                    <AiOutlineClose fontSize={28} className="text-white cursor-pointer md:hidden" onClick={() => {setToggleMenu(false)}}/> 
+                :
+                    <HiMenuAlt4 fontSize={28} className="text-white cursor-pointer md:hidden" onClick={() => {setToggleMenu(true)}}/>
+                    
+                }
+                {toggleMenu && (
+                    <ul className="z-10 flex top-0 right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide">
+                        <li className="text-xl w-full my-2">
+                            <AiOutlineClose onClick={() => setToggleMenu(false)}/>
+                        </li>
+                        {["Marché", "Echanges", "Coures", "Port-feullies"].map( (item, id) => (
+                        <NavItem key={item + id} title={item} Properties="my-2 text-lg"/>
+                    ) )}
+                
+                    </ul>
+                )}
+            </div>
         </nav>
+        
     );
 }
 
